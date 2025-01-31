79876d2a419463e8648d095bdb66a4218b42ca71
Phillip England <phillip.mark.england@gmail.com>
2025-01-31 05:59:23 -0600

    added CORS middleware

--- vbf.go
+++ vbf.go
@@ -63,6 +63,21 @@ func MwLogger(next http.Handler) http.Handler {
 	})
 }
 
+// MwCORS is a middleware that allows cross-origin requests.
+func MwCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 //=====================================
 // PROVIDED HANDLERS
 //=====================================
